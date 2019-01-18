cf81e7734de88bbac3c5d9f0002ecccef244716d
Admir Trakic <xomodo@gmail.com>
2019-01-19 00:04:18 +0100

    Use docker

--- terraform-common.mk
+++ terraform-common.mk
@@ -1,65 +1,66 @@
 SHELL := /bin/bash
-
-TERRAFORM := $(shell git rev-parse --show-toplevel)/terraform
-TMP ?= /tmp
-OS ?= $(shell uname -s | tr '[:upper:]' '[:lower:]')
-TERRAFORM_VERSION ?= 0.11.10
-TERRAFORM_URL ?= https://releases.hashicorp.com/terraform/$(TERRAFORM_VERSION)/terraform_$(TERRAFORM_VERSION)_$(OS)_amd64.zip
+DOCKER_IMAGE := hashicorp/terraform:light
 
 .PHONY: install
-install: ## Install terraform
-	@[ -x $(TERRAFORM) ] || ( \
-		echo "Installing terraform $(TERRAFORM_VERSION) ($(OS)) from $(TERRAFORM_URL)" && \
-		curl '-#' -fL -o $(TMP)/terraform.zip $(TERRAFORM_URL) && \
-		unzip -q -d $(TMP)/ $(TMP)/terraform.zip && \
-		mv $(TMP)/terraform $(TERRAFORM) && \
-		rm -f $(TMP)/terraform.zip \
-		)
-
-.PHONY: apply
-apply: init ## Builds or changes infrastructure
-	@$(TERRAFORM) apply -auto-approve
-
-.PHONY: graph
-graph: apply ## Create a visual graph of Terraform resources
-	@$(TERRAFORM) graph
-
-.PHONY: state-pull
-state-pull: ## Pull the state from its location and output it to stdout
-	@$(TERRAFORM) state pull
-
-.PHONY: state-list
-state-list: ## List resources in the Terraform state
-	@$(TERRAFORM) state list
-
-.PHONY: state-show
-state-show: ## Shows the attributes of a resource in the Terraform state
-	@$(TERRAFORM) state show
-
-.PHONY: output
-output: apply ## Read an output from a state file
-	@$(TERRAFORM) output
-
-.PHONY: version
-version: install ## Prints the Terraform version
-	@$(TERRAFORM) version
+install: ## Pull latest images
+	@docker image inspect $(DOCKER_IMAGE) >/dev/null 2>&1 || docker pull $(DOCKER_IMAGE)
 
 .PHONY: init
 init: install ## Initilise empty terraform env
-	@$(TERRAFORM) init -input=false >/dev/null
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		init -input=false >/dev/null
+
+.PHONY: apply
+apply: init ## Builds or changes infrastructure
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		apply -auto-approve
+
+.PHONY: graph
+graph: apply ## Create a visual graph of Terraform resources
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		graph
+
+.PHONY: state-pull
+state-pull: ## Pull the state from its location and output it to stdout
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		state pull
+
+.PHONY: state-list
+state-list: ## List resources in the Terraform state
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		state list
+
+.PHONY: state-show
+state-show: ## Shows the attributes of a resource in the Terraform state
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		state show
+
+.PHONY: output
+output: apply ## Read an output from a state file
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		output
+
+.PHONY: version
+version: ## Prints the Terraform version
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		version
 
 .PHONY: validate
 validate: ## Validates the Terraform files
-	@$(TERRAFORM) validate -check-variables=false .
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		validate -check-variables=false .
 
 .PHONY: show
 show: apply ## Inspect Terraform state or plan
-	@$(TERRAFORM) show
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		show
 
 .PHONY: fmt
 fmt: ## Rewrites config files to canonical format
-	@$(TERRAFORM) fmt
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		fmt
 
 .PHONY: clean
-clean: ## Clean up
-	@rm -rf *.tfstate *.tfstate.backup $(TERRAFORM) .terraform/* >/dev/null
+clean: ## Destroy Terraform-managed infrastructure
+	@docker run -it --rm -w $$PWD -v $$PWD:$$PWD $(DOCKER_IMAGE) \
+		destroy -auto-approve
