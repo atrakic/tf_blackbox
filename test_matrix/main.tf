4dc5d10c7870d3e8d19f71241ed7d70b8d6b9cd5
Admir Trakic <xomodo@gmail.com>
2019-02-28 17:45:07 +0100

    Testing matrix

--- test_matrix/main.tf
+++ test_matrix/main.tf
@@ -1,45 +1,33 @@
 variable "matrix" {
   type = "map"
+
   default = {
-    chars   = "a,"
-    numbers = "1,"
-    tld     = "com,"
+    chars   = "a,b,c"
+    numbers = "1,2,3"
+    tld     = "com,net,org"
   }
+
+  description = "Matrix is a rectangular array of numbers, symbols, or expressions, arranged in rows and columns."
 }
 
 resource "null_resource" "test_matrix" {
   count = "${length(split(",",var.matrix["chars"]))}"
 
   triggers {
-    chars        = "${element(split(",", var.matrix["chars"]), count.index)}"
-    numbers      = "${element(split(",", var.matrix["numbers"]), count.index)}"
-    tld          = "${element(split(",", var.matrix["tld"]), count.index)}"
+    chars   = "${element(split(",", var.matrix["chars"]), count.index)}"
+    numbers = "${element(split(",", var.matrix["numbers"]), count.index)}"
+    tlds    = "${element(split(",", var.matrix["tld"]), count.index)}"
   }
-}
-
-
-/*
-data "null_data_source" "values" {
-  # FIXME: https://www.terraform.io/docs/providers/null/data_source.html
-  count = "${length(split(",",var.matrix["chars"]))}"
-  inputs = {
-    all_chars = "${null_resource.test_matrix.*.id}"
-  }
-}
-locals {
-  # FIXME:
-  ssm_dbs     = "${data.null_resource.test_matrix[0]"
-  ssm_users   = "${data.aws_ssm_parameter.dbuser.value}"
-  ssm_passwds = "${data.aws_ssm_parameter.dbpass.value}"
 }
 
 output "chars" {
-  # FIXME: https://www.terraform.io/docs/providers/null/data_source.html
-  //value       = "${data.null_data_source.values.outputs["all_chars"]}"
-  value       = "${data.null_data_source.values.*.outputs}" 
+  value = "${null_resource.test_matrix.*.triggers.chars}"
 }
 
-test: 
-  terraform state show "null_resource.test_matrix[0]"
+output "numbers" {
+  value = "${null_resource.test_matrix.*.triggers.numbers}"
+}
 
-*/
+output "tlds" {
+  value = "${null_resource.test_matrix.*.triggers.tlds}"
+}
